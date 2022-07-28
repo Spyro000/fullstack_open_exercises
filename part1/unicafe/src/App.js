1ed0eb91d56d1451cca1a44a0cfda38edd831b53
SpyFox <wasyldykun@gmail.com>
2022-07-28 12:28:02 +0100

    1.7 finished

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -22,6 +22,9 @@ const Statistic = ({good, neutral, bad}) => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good - bad) / (good + neutral + bad)}</div>
+      <div>positive {good * 100 / (good + neutral + bad)} %</div>
     </div>
   )
 }
