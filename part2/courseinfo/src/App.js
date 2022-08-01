f6933d75f4703e849e26945dec4004e4733f8642
SpyFox <wasyldykun@gmail.com>
2022-08-01 11:56:16 +0100

    2.1 finished

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,69 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+    //<Total parts={course.parts} />
+  )
+}
+
+const Header = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) =>
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  return (
+    <div>
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
