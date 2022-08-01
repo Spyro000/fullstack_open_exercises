9f9dabd1385cd9a3356bb8b8ded3c324a99ae749
SpyFox <wasyldykun@gmail.com>
2022-08-01 12:20:57 +0100

    2.5 finished

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,50 @@
+const Header = ({ name }) => {
+    return (
+        <h2>{name}</h2>
+    )
+}
+
+const Content = ({ parts }) => {
+    return (
+        <div>
+            {parts.map((part) =>
+                <Part key={part.id} part={part} />
+            )}
+        </div>
+    )
+}
+
+const Total = ({ parts }) => {
+    const total =
+        parts.reduce((s, p) => s + p.exercises, 0)
+
+    return (
+        <div>
+            <p>
+                <b>
+                    total of {total} exercises
+                </b>
+            </p>
+        </div>
+    )
+}
+
+const Part = ({ part }) => {
+    return (
+        <p>
+            {part.name} {part.exercises}
+        </p>
+    )
+}
+
+const Course = ({ course }) => {
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+export default Course
