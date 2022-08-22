3d8a3cebd561e62caf76924bfe4eaa1c1d1e7a20
SpyFox <wasyldykun@gmail.com>
2022-08-22 10:25:43 +0100

    4.5 finished

--- part4/blog/tests/list_Helper.test.js
+++ part4/blog/tests/list_Helper.test.js
@@ -1,23 +1,34 @@
-const { dummy, totalLikes } = require('../utils/list_helper');
+const listHelper = require('../utils/list_helper');
 const blogs = require('./test_blogs');
 
+// dummy tests
 test('dummy returns 1', () => {
   const empty = [];
 
-  expect(dummy(empty)).toBe(1);
+  expect(listHelper.dummy(empty)).toBe(1);
 });
 
+// totalLikes tests
 describe('total likes', () => {
   test(
     'of empty list is zero',
-    () => expect(totalLikes([])).toBe(0),
+    () => expect(listHelper.totalLikes([])).toBe(0),
   );
   test(
     'when list has only one blog equals of that',
-    () => expect(totalLikes([blogs[0]])).toBe(7),
+    () => expect(listHelper.totalLikes([blogs[0]])).toBe(7),
   );
   test(
     'of a bigger list is calculated right',
-    () => expect(totalLikes(blogs)).toBe(36),
+    () => expect(listHelper.totalLikes(blogs)).toBe(36),
+  );
+});
+
+// favoriteBlog tests
+
+describe('favorite blog', () => {
+  test(
+    'choose blog with the most likes in array',
+    () => expect(listHelper.favoriteBlog(blogs)).toBe(blogs[2]),
   );
 });
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -3,4 +3,16 @@ const dummy = () => 1;
 const totalLikes = (posts = []) => posts
   .reduce((total, post) => total + post.likes, 0);
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (posts = []) => posts
+  .reduce((mostPopular, current) => {
+    const result = current.likes > mostPopular.likes
+      ? current
+      : mostPopular;
+    return result;
+  }, posts[0]);
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+};
