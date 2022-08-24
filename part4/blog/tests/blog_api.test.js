636e7747785e40695fcef153d9c53b72372bf1a8
SpyFox <wasyldykun@gmail.com>
2022-08-24 12:34:06 +0100

    4.11 finished

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -36,7 +36,15 @@ describe('GET /api/blogs ', () => {
 });
 
 describe('POST /api/blogs', () => {
-  test('should add new blog', async () => {
+  test('should return created blog responce with id', async () => {
+    const testBlog = helper.getTestBlog();
+    const responce = await api
+      .post('/api/blogs')
+      .send(testBlog);
+    expect(responce.body.id).toBeDefined();
+  });
+
+  test('should increase amount of blogs by 1', async () => {
     const testBlog = helper.getTestBlog();
 
     await api
@@ -44,15 +52,50 @@ describe('POST /api/blogs', () => {
       .send(testBlog);
 
     const responce = await api.get('/api/blogs');
-    const content = responce.body.map((blog) => {
-      const entity = { ...blog };
-      delete entity.id;
-      return entity;
-    });
 
-    expect(content.length)
+    expect(responce.body.length)
       .toBe(helper.initialBlogs.length + 1);
-    expect(content).toContainEqual(testBlog);
+  });
+
+  test('id of POST responce should exists in DB', async () => {
+    const testBlog = helper.getTestBlog();
+    const postResponce = await api
+      .post('/api/blogs')
+      .send(testBlog);
+    const idOfResponce = postResponce.body.id;
+
+    const getResponce = await api.get('/api/blogs');
+    const allID = getResponce.body.map((blog) => blog.id);
+
+    expect(allID).toContain(idOfResponce);
+  });
+
+  test('should save all data of request under correct ID', async () => {
+    const testBlog = helper.getTestBlog();
+    const postResponce = await api
+      .post('/api/blogs')
+      .send(testBlog);
+    const idOfResponce = postResponce.body.id;
+
+    const getResponce = await api.get('/api/blogs');
+    const savedBlog = getResponce.body.find((blog) => blog.id === idOfResponce);
+
+    expect(savedBlog).toEqual(expect.objectContaining(testBlog));
+  });
+
+  test('should set likes to 0 if no such field provided', async () => {
+    const testBlog = helper.getTestBlog();
+    delete testBlog.likes;
+    const postResponce = await api
+      .post('/api/blogs')
+      .send(testBlog);
+    const idOfResponce = postResponce.body.id;
+
+    const getResponce = await api.get('/api/blogs');
+    const savedBlog = getResponce.body.find((blog) => blog.id === idOfResponce);
+
+    expect(savedBlog.likes).toBeDefined();
+    expect(savedBlog.likes).toBe(0);
   });
 });
 
