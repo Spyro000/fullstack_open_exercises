f5f98fef8fb61fe86bcf6d19d92ea28194ef50b7
SpyFox <wasyldykun@gmail.com>
2022-08-29 11:00:39 +0100

    4.17 finished

--- part4/blog/controllers/blog.js
+++ part4/blog/controllers/blog.js
@@ -1,20 +1,42 @@
+/* eslint-disable no-underscore-dangle */
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogRouter.post('/', async (request, response) => {
-  if (!request.body.url || !request.body.title) {
+  const {
+    url,
+    title,
+    userId,
+    author,
+    likes,
+  } = request.body;
+
+  if (!url || !title || !userId) {
     response.status(400).end();
     return;
   }
 
-  const blog = new Blog(request.body);
-  const result = await blog.save();
-  response.status(201).json(result);
+  const user = await User.findById(userId);
+
+  const blog = new Blog({
+    url,
+    title,
+    user: user._id,
+    author,
+    likes,
+  });
+  const savedBlog = await blog.save();
+
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
+  response.status(201).json(savedBlog);
 });
 
 blogRouter.put('/:id', async (request, responce) => {
--- part4/blog/controllers/user.js
+++ part4/blog/controllers/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 userRouter.get('/', async (request, responce) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1 });
   responce.json(users);
 });
 
