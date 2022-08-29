6e662f7fc2da9a369127a10a8cedd1e7014aa94b
SpyFox <wasyldykun@gmail.com>
2022-08-29 11:32:44 +0100

    4.18 finished

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const blogRouter = require('./controllers/blog');
 const userRouter = require('./controllers/user');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const { MONGODB_URI } = require('./utils/config');
 const { info, error } = require('./utils/logger');
@@ -28,6 +29,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
