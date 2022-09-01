fd8a9926bccc79fd0f06688b3d3849e2c651082e
SpyFox <wasyldykun@gmail.com>
2022-09-01 12:21:30 +0100

    5.1 added es-lint

---
+++ part5/bloglist-frontend-main/src/components/Blog.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+function Blog({ blog }) {
+  return (
+    <div>
+      {blog.title}
+      {blog.author}
+    </div>
+  );
+}
+Blog.propTypes = {
+  blog: PropTypes.shape({
+    title: PropTypes.string,
+    author: PropTypes.string,
+  }).isRequired,
+
+};
+
+export default Blog;
--- part5/bloglist-frontend-main/src/services/blogs.js
+++ part5/bloglist-frontend-main/src/services/blogs.js
@@ -1,9 +1,10 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const baseUrl = '/api/blogs';
 
-export default { getAll }
+const getAll = async () => {
+  const responce = await axios.get(baseUrl);
+  return responce.data;
+};
+
+export default { getAll };
