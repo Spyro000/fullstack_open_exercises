6e82d3e20b5d1c5926b6749b27b474e92b492bc7
SpyFox <wasyldykun@gmail.com>
2022-09-24 10:48:25 +0100

    5.18 finished

--- part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
@@ -4,7 +4,7 @@ describe('Blog app', function () {
   beforeEach(function () {
     const baseUser = {
       username: 'username',
-      user: 'user',
+      name: 'user',
       password: 'password',
     };
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
@@ -17,5 +17,23 @@ describe('Blog app', function () {
     cy.contains('username');
     cy.contains('password');
     cy.contains('login');
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('[type="text"]').type('username');
+      cy.get('[type="password"]').type('password');
+      cy.contains('login').click();
+
+      cy.contains('user logged in');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('[type="text"]').type('1username');
+      cy.get('[type="password"]').type('1password');
+      cy.contains('login').click();
+
+      cy.contains('invalid username or password').should('have.css', 'color', 'rgb(255, 0, 0)');
+    });
   });
 });
